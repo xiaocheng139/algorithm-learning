58e79000dd68c3e15c8cf778d3843c7f5005458c
Xiaocheng Fei <61fxc.job@gmail.com>
2020-02-06 04:35:32 +0000

    Continue learning binary search

---
+++ leetcode/Binary Search/278. First Bad Version.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode id=278 lang=python3
+#
+# [278] First Bad Version
+#
+# https://leetcode.com/problems/first-bad-version/description/
+#
+# algorithms
+# Easy (32.73%)
+# Likes:    938
+# Dislikes: 539
+# Total Accepted:    291.1K
+# Total Submissions: 888.7K
+# Testcase Example:  '5\n4'
+#
+# You are a product manager and currently leading a team to develop a new
+# product. Unfortunately, the latest version of your product fails the quality
+# check. Since each version is developed based on the previous version, all the
+# versions after a bad version are also bad.
+# 
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first
+# bad one, which causes all the following ones to be bad.
+# 
+# You are given an API bool isBadVersion(version) which will return whether
+# version is bad. Implement a function to find the first bad version. You
+# should minimize the number of calls to the API.
+# 
+# Example:
+# 
+# 
+# Given n = 5, and version = 4 is the first bad version.
+# 
+# call isBadVersion(3) -> false
+# call isBadVersion(5) -> true
+# call isBadVersion(4) -> true
+# 
+# Then 4 is the first bad version. 
+# 
+# 
+#
+
+# @lc code=start
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 0
+        end = n - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if not isBadVersion(mid + 1):
+                start = mid
+            else:
+                end = mid
+        
+        if not isBadVersion(start + 1):
+            return end + 1
+        else:
+            return start + 1
+        
+
+        
+# @lc code=end
