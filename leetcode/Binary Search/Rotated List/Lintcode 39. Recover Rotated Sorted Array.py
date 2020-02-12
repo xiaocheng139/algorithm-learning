79853e3ea2f5662d7f06b6f28af96e938c936b25
jackfei01 <48191365+jackfei01@users.noreply.github.com>
2020-02-12 16:38:30 +1100

    BS Continue

---
+++ leetcode/Binary Search/Rotated List/Lintcode 39. Recover Rotated Sorted Array.py
@@ -0,0 +1,55 @@
+# 39. Recover Rotated Sorted Array
+
+# Given a rotated sorted array, recover it to sorted array in-place.（Ascending）
+#
+# Example
+# Example1:
+# [4, 5, 1, 2, 3] -> [1, 2, 3, 4, 5]
+# Example2:
+# [6,8,9,1,2] -> [1,2,6,8,9]
+#
+# Challenge
+# In-place, O(1) extra space and O(n) time.
+#
+# Clarification
+# What is rotated array?
+#
+# For example, the orginal array is [1,2,3,4], The rotated array of it can be [1,2,3,4], [2,3,4,1], [3,4,1,2], [4,1,2,3]
+
+class Solution:
+    """
+    @param nums: An integer array
+    @return: nothing
+    """
+    def recoverRotatedSortedArray(self, nums):
+        # Find the lowest number
+        start = 0
+        end = len(nums) - 1
+
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if nums[mid] > nums[end]:
+                start = mid
+            else:
+                end = mid
+
+        minimum_index = end if nums[start] > nums[end] else start
+
+
+
+    # Define a function that reverse the target list
+    def reverse(self, nums):
+        if len(nums) <= 1:
+            return nums
+
+        i, j = 0, len(nums) -1
+
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+
+        return nums
+
+solution = Solution()
+print(solution.reverse([1,2,3,4,5,6,7,8]))
